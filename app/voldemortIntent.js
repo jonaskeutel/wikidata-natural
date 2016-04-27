79c8d9caa571310c4b5dfeafa1e4010f9cd096fd
Jonas Keutel <jonas@keutel.de>
2016-04-27 20:21:11 +0200

    adds voldemortIntent as easterEgg for Stefan, Henni, ...

---
+++ app/voldemortIntent.js
@@ -0,0 +1,23 @@
+exports.Intent = function(data, classifier){
+  var intent = require('./Intent').Intent(data['voldemort'], classifier, 'voldemort');
+
+  var parse = function(question) {
+		return {'searchText': 'Lord Voldemort'};
+  };
+
+  var doQuery = function(data, callback) {
+		data.speechOutput = 'Yes, Rosa and Jonas are right: He wanted to keep his return secret. His idea was to gain power and has everything under control before he reveals his return to the public.';
+    callback(null, data);
+  };
+
+  var getInterpretation = function(data, callback) {
+    data.interpretation = 'Did Lord Voldemort want to keep his return secret?';
+    callback(null, data);
+  }
+
+  intent.parse = parse;
+  intent.doQuery = doQuery;
+  intent.getInterpretation = getInterpretation;
+
+  return intent;
+}

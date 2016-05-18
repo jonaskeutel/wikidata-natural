c34602924b3acc4991c81765350899f7f1c132d2
Florian Meinel <florian.meinel@student.hpi.uni-potsdam.de>
2016-05-18 15:20:50 +0200

    extend date formatter

--- app/dynamicQuestionAnswering/answerFormatter.js
+++ app/dynamicQuestionAnswering/answerFormatter.js
@@ -1,19 +1,44 @@
 "use strict";
 
 exports.formatAnswer = function(property, namedEntity, result) {
-	var answer = "The " + property.label + " of " + namedEntity.label + " is " + formatResult(result) + ".";
+	var answer = "The " + property.label + " of " + namedEntity.label + formatIs(result) + formatResult(result) + ".";
 	return answer;
 };
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function formatIs(result) {
+	if (datatypeOf(result) == 'dateTime') {
+		var d = new Date(result.objectLabel.value);
+		var now = new Date();
+		if (d < now) {
+			return " was on ";
+		} else {
+			return " will be on ";
+		}
+	}
+	return " is ";
 }
 
 function formatResult(result) {
-	if (result.object.datatype == 'http://www.w3.org/2001/XMLSchema#dateTime') {
-		return formatDate(result.objectLabel.value);
+	var formatFunction = selectFunctionForDatatype(datatypeOf(result));
+	if (formatFunction) {
+		return formatFunction(result.objectLabel.value);
 	}
 	return result.objectLabel.value;
+}
+
+function datatypeOf(result) {
+	if (!result.object || !result.object.datatype || result.object.datatype.indexOf('#') == -1) {
+		return null;
+	}
+	return result.object.datatype.substring(result.object.datatype.indexOf('#') + 1);
+}
+
+function selectFunctionForDatatype(datatype) {
+	var formatFunctions = {
+		"decimal": formatDecimal,
+		"dateTime": formatDate
+	};
+	return formatFunctions[datatype];
 }
 
 function formatDate(dateLiteral) {
@@ -21,5 +46,83 @@ function formatDate(dateLiteral) {
     var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    return weekdays[d.getDay()] + ", " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+    if (isNaN(d.getTime())) {
+    	return dateLiteral;
+    }
+
+    var oneDay = 24*60*60*1000;
+
+    return weekdays[d.getDay()] + ", " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear() + timeSpanSince(d);
+}
+
+function timeSpanSince(aDate) {
+	var oneDay = 24*60*60*1000;
+	var oneWeek = 7 * oneDay;
+	var oneMonth = 30.44 * oneDay;
+	var oneYear = 365.25 * oneDay;
+
+	var timeSpanString = " (about ";
+
+	var diff = Math.abs(new Date().getTime() - aDate.getTime());
+	var diffNormalized = diff;
+	var unit = "";
+	if (diff > oneYear * 1.3) {
+		diffNormalized = Math.floor(diff / oneYear);
+		unit = "year";
+	} else if (diff > oneMonth * 1.3) {
+		diffNormalized = Math.floor(diff / oneMonth);
+		unit = "month";
+	} else if (diff > oneWeek * 1.3) {
+		diffNormalized = Math.floor(diff / oneWeek);
+		unit = "week";
+	} else if (diff > oneDay * 1.3) {
+		diffNormalized = Math.floor(diff / oneDay);
+		unit = "day";
+	} else {
+		return "";
+	}
+	timeSpanString += diffNormalized + " " + unit;
+	if (diffNormalized > 1) {
+		timeSpanString += "s";
+	}
+	if (new Date() > aDate) {
+		timeSpanString += " ago)";
+	} else {
+		timeSpanString += " from now)";
+	}
+
+	return timeSpanString;
+}
+
+function formatDecimal(decimalLiteral) {
+	var num = Number(decimalLiteral);
+	var suffix = "";
+	if (num % 1000000000 === 0) {
+		num /= 1000000000;
+		suffix += " billion";
+	} else if (num % 1000000 === 0) {
+		num /= 1000000;
+		suffix += " million";
+	}
+	var str = num.toString();
+	var leftOfDecimalPoint = str.split('.')[0].split("");
+	var rightOfDecimalPoint = str.split('.')[1];
+	leftOfDecimalPoint = leftOfDecimalPoint;
+	var formatted = "";
+	for (var i = leftOfDecimalPoint.length - 1; i >= 0; i--) {
+		formatted = leftOfDecimalPoint[i] + formatted;
+		if ( (leftOfDecimalPoint.length - i) % 3 === 0 && isNumeric(leftOfDecimalPoint[i - 1])) {
+			formatted = " " + formatted;
+		}
+	}
+	if (rightOfDecimalPoint) {
+		formatted += '.';
+		formatted += rightOfDecimalPoint;
+	}
+
+	return formatted;
+}
+
+function isNumeric(char) {
+	return !isNaN(parseInt(char));
 }
